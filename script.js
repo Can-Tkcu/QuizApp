a72b9eda4c131136eff28c3c11af7312d70d1340
Can-Tkcu <can.tkcu@hotmail.com>
2022-08-18 12:59:43 +0200

    added card and JSON Array template

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question": "",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "",
+        "answer_4": "",
+        "right_answer": 3
+    },
+    {
+        "question": "",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "",
+        "answer_4": "",
+        "right_answer": 3
+    },
+    {
+        "question": "",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "",
+        "answer_4": "",
+        "right_answer": 3
+    },
+    {
+        "question": "",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "",
+        "answer_4": "",
+        "right_answer": 3
+    },
+    {
+        "question": "",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "",
+        "answer_4": "",
+        "right_answer": 3
+    },
+];
