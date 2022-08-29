8f55b5519f3c39b8b1b217ec2b7551dd0891aee1
Can-Tkcu <can.tkcu@hotmail.com>
2022-08-29 20:39:16 +0200

    finalized QuizApp

--- script.js
+++ script.js
@@ -18,13 +18,10 @@ let questionsA = [
   },
   {
     question: "Was ist der Unterschied zwischen Java und JavaScript?",
-    answer_1:
-      "Java ist eine OOP-Programmiersprache, während JavaScript eine OOP-Skriptingsprache ist",
-    answer_2:
-      "Java wurde nur für Minecraft entwickelt, während JavaScript universell ist",
+    answer_1: "Java ist eine OOP-Programmiersprache, während JavaScript eine OOP-Skriptingsprache ist",
+    answer_2: "Java wurde nur für Minecraft entwickelt, während JavaScript universell ist",
     answer_3: "JavaScript und Java sind beide gleich",
-    answer_4:
-      "Java ist der große Bruder von JavaScript, daher sind beide fast gleich",
+    answer_4: "Java ist der große Bruder von JavaScript, daher sind beide fast gleich",
     right_answer: 1,
   },
   {
@@ -38,8 +35,7 @@ let questionsA = [
   {
     question: "Was macht eine for-Schleife?",
     answer_1: "Eine Methode um sich die Schuhe zu zubinden",
-    answer_2:
-      "For-Schleifen sind ein einfaches Werkzeug, um einzelne Schritte wiederholt auszuführen",
+    answer_2: "For-Schleifen sind ein einfaches Werkzeug, um einzelne Schritte wiederholt auszuführen",
     answer_3: "Eine for-Schleife bindet externe Webseiten ein",
     answer_4: "Mit der for-Schleife kann man Krawattenbindeprogramme coden",
     right_answer: 2,
@@ -91,10 +87,6 @@ let questionsB = [
 ];
 
 
-/**TODO
- * responsive 
- * finish end screen
- */
 let rightAnsweredQuestions = 0;
 
 let currentQuestion = 0;
@@ -107,112 +99,36 @@ let AUDIO_BEGIN = new Audio("audio/begin.mp3")
 
 let AUDIO_END = new Audio("audio/end.mp3")
 
-///////////////////////////////////////////////////////////////////////////////////JAVASCRIPT QUIZ////////////////////////////////////////////////////////////////////////////////////
+let JS = questionsA;
+let GM = questionsB;
+
+///////////////////////////////////////////////////////////////////////////////////JAVASCRIPT QUIZ////////////////////////////////////////////////////////////////////////////////////////
 
 function initA() {
   showQuiz();
-  showQuestionA();
+  showQuestion(JS);
 }
 
-function showQuestionA() {
-  let question = questionsA[currentQuestion];
-  if (gameIsOverA()) {
-      showEndScreen();
-  } else {
-      displayCurrentQuestion(question);
-      updateQuestionA();
-      updateProgressBarA();
-  }
-}
-
-function selected(answer) {
-  let question = questionsA[currentQuestion];
-  let selectedQuestionNumber = answer.slice(-1);
-  let correctAnswerId = `answer_${question["right_answer"]}`;
-  if (selectedQuestionNumber == question["right_answer"]) {
-    AUDIO_SUCCESS.play();
-    document.getElementById(answer).parentNode.classList.add("bg-success");
-    rightAnsweredQuestions++;
-  } else {
-    AUDIO_FAIL.play();
-    document.getElementById(answer).parentNode.classList.add("bg-danger");
-    document.getElementById(correctAnswerId).parentNode.classList.add("bg-success");
-  }
-  disableSelection();
-  document.getElementById("next-question").disabled = false;
-}
-
-function nextQuestionA() {
-  currentQuestion++;
-  showQuestionA();
-  resetCard();
-  enableSelection();
-  document.getElementById("next-question").disabled = true;
-}
-
-function resetCard() {
-  document.getElementById("answer_1").parentNode.classList.remove("bg-success");
-  document.getElementById("answer_1").parentNode.classList.remove("bg-danger");
-  document.getElementById("answer_2").parentNode.classList.remove("bg-success");
-  document.getElementById("answer_2").parentNode.classList.remove("bg-danger");
-  document.getElementById("answer_3").parentNode.classList.remove("bg-success");
-  document.getElementById("answer_3").parentNode.classList.remove("bg-danger");
-  document.getElementById("answer_4").parentNode.classList.remove("bg-success");
-  document.getElementById("answer_4").parentNode.classList.remove("bg-danger");
-}
-
-function restartGameA() {
-  document.getElementById("quiz-start").classList.remove("d-none");
-  document.getElementById("quiz-end").classList.add("d-none");
-  rightAnsweredQuestions = 0;
-  currentQuestion = 0;
+/**initiates JS Quiz */
+function SelectQuizA(){
+  document.getElementById('start-quiz').classList.add('d-none');
   initA();
+  AUDIO_BEGIN.play();
 }
 
-function showEndScreen(){
-    AUDIO_END.play();
-    document.getElementById("quiz-start").classList.add("d-none");
-    document.getElementById("quiz-end").classList.remove("d-none");
-    document.getElementById("quiztotal-end").innerHTML = questionsA.length;
-    document.getElementById("end-score-correct-answers").innerHTML =  rightAnsweredQuestions;
-}
-
-function updateProgressBarA(){
-  let percent = (currentQuestion) / questionsA.length;
-  percent = percent * 100;
-  document.getElementById("progress-bar").innerHTML = `${percent}%`;
-  document.getElementById("progress-bar").style = `width: ${percent}%`;
-}
- 
-function updateQuestionA(){
-  document.getElementById("quiztotal").innerHTML = questionsA.length;
-  document.getElementById("current-question").innerHTML = currentQuestion + 1;
-}
-
-function displayCurrentQuestion(question){
-  document.getElementById("questiontext").innerHTML = question["question"];
-  document.getElementById("answer_1").innerHTML = question["answer_1"];
-  document.getElementById("answer_2").innerHTML = question["answer_2"];
-  document.getElementById("answer_3").innerHTML = question["answer_3"];
-  document.getElementById("answer_4").innerHTML = question["answer_4"];
-}
-
-function gameIsOverA(){
-  return currentQuestion >= questionsA.length;
-}
-
-function disableSelection(){
-  document.getElementById("answer_a").style.pointerEvents = 'none';
-  document.getElementById("answer_b").style.pointerEvents = 'none';
-  document.getElementById("answer_c").style.pointerEvents = 'none';
-  document.getElementById("answer_d").style.pointerEvents = 'none';
-}
-
-function enableSelection(){
-  document.getElementById("answer_a").style.pointerEvents = 'auto'; 
-  document.getElementById("answer_b").style.pointerEvents = 'auto'; 
-  document.getElementById("answer_c").style.pointerEvents = 'auto'; 
-  document.getElementById("answer_d").style.pointerEvents = 'auto'; 
+/**selects JS Quiz */
+function selectA(){
+  document.getElementById("quiz-button").setAttribute( "onClick", "SelectQuizA()" );
+  document.getElementById("next-question").setAttribute( "onClick", "nextQuestion(JS)" );
+  document.getElementById("restart-button").setAttribute( "onClick", "restartGame(initA)" );
+  document.getElementById("answer_a").setAttribute( "onClick", "selected('answer_1', JS)" );
+  document.getElementById("answer_b").setAttribute( "onClick", "selected('answer_2', JS)" );
+  document.getElementById("answer_c").setAttribute( "onClick", "selected('answer_3', JS)" );
+  document.getElementById("answer_d").setAttribute( "onClick", "selected('answer_4', JS)" );
+  document.getElementById("JS").classList.add('selected');
+  document.getElementById("GM").classList.remove('selected');
+  document.getElementById("quiz-type").innerHTML = "JavaScript Quiz beendet!";
+  document.getElementById("quiz-button").disabled = false;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////JAVASCRIPT QUIZ END////////////////////////////////////////////////////////////////////////////////////
@@ -222,25 +138,39 @@ function enableSelection(){
 
 function initB() {
   showQuiz();
-  showQuestionB();
+  showQuestion(GM);
 }
 
-function showQuestionB() {
-  let question = questionsB[currentQuestion];
-  if (gameIsOverB()) {
-      showEndScreen();
-  } else {
-      displayCurrentQuestion(question);
-      updateQuestionB();
-      updateProgressBarB();
-  }
+/**initiates Games Quiz */
+function SelectQuizB(){
+  document.getElementById('start-quiz').classList.add('d-none');
+  initB();
+  AUDIO_BEGIN.play();
+  
 }
 
-function selected(answer) {
-  let question = questionsB[currentQuestion];
+/**selects Games Quiz */
+function selectB(){
+  document.getElementById("quiz-button").setAttribute( "onClick", "SelectQuizB()" );
+  document.getElementById("next-question").setAttribute( "onClick", "nextQuestion(GM)" );
+  document.getElementById("restart-button").setAttribute( "onClick", "restartGame(initB)" );
+  document.getElementById("answer_a").setAttribute( "onClick", "selected('answer_1', GM)" );
+  document.getElementById("answer_b").setAttribute( "onClick", "selected('answer_2', GM)" );
+  document.getElementById("answer_c").setAttribute( "onClick", "selected('answer_3', GM)" );
+  document.getElementById("answer_d").setAttribute( "onClick", "selected('answer_4', GM)" );
+  document.getElementById("GM").classList.add('selected');
+  document.getElementById("JS").classList.remove('selected');
+  document.getElementById("quiz-type").innerHTML = "Games Quiz beendet!";
+  document.getElementById("quiz-button").disabled = false;
+}
+
+///////////////////////////////////////////////////////////////////////////////////GAMES QUIZ END/////////////////////////////////////////////////////////////////////////////////////////
+
+function selected(answer, question) {
+  let rightAnswer = question[currentQuestion]["right_answer"];
   let selectedQuestionNumber = answer.slice(-1);
-  let correctAnswerId = `answer_${question["right_answer"]}`;
-  if (selectedQuestionNumber == question["right_answer"]) {
+  let correctAnswerId = `answer_${rightAnswer}`;
+  if (selectedQuestionNumber == rightAnswer) {
     AUDIO_SUCCESS.play();
     document.getElementById(answer).parentNode.classList.add("bg-success");
     rightAnsweredQuestions++;
@@ -253,14 +183,98 @@ function selected(answer) {
   document.getElementById("next-question").disabled = false;
 }
 
-function nextQuestionB() {
+function nextQuestion(quiztype) {
   currentQuestion++;
-  showQuestionB();
+  showQuestion(quiztype);
   resetCard();
   enableSelection();
   document.getElementById("next-question").disabled = true;
 }
 
+function showQuestion(quiztype) {
+  if (gameIsOver()) {
+      showEndScreen();
+  } else {
+    if(quiztype == questionsA) {
+      displayCurrentQuestion(questionsA[currentQuestion]);
+    } else {
+      displayCurrentQuestion(questionsB[currentQuestion]);
+    }
+      updateQuestion();
+      updateProgressBar();
+  }
+}
+
+function displayCurrentQuestion(question){
+  document.getElementById("questiontext").innerHTML = question["question"];
+  document.getElementById("answer_1").innerHTML = question["answer_1"];
+  document.getElementById("answer_2").innerHTML = question["answer_2"];
+  document.getElementById("answer_3").innerHTML = question["answer_3"];
+  document.getElementById("answer_4").innerHTML = question["answer_4"];
+}
+
+function updateQuestion(){
+  document.getElementById("quiztotal").innerHTML = questionsA.length;
+  document.getElementById("current-question").innerHTML = currentQuestion + 1;
+}
+
+function updateProgressBar(){
+  let percent = (currentQuestion) / questionsA.length;
+  percent = percent * 100;
+  document.getElementById("progress-bar").innerHTML = `${percent}%`;
+  document.getElementById("progress-bar").style = `width: ${percent}%`;
+}
+
+function restartGame(init) {
+  document.getElementById("quiz-start").classList.remove("d-none");
+  document.getElementById("quiz-end").classList.add("d-none");
+  ResetVarsToZero();
+  init();
+}
+
+/**game over listener*/
+function gameIsOver(){
+  return currentQuestion >= questionsA.length;
+}
+
+/**Returns the User back to the starting screen */
+function returnToStart(){
+  document.getElementById("quiz-end").classList.add("d-none");
+  document.getElementById("quiz-start").classList.add("d-none");
+  document.getElementById("javascript-quiz").classList.add("d-none");
+  document.getElementById("start-quiz").classList.remove("d-none");
+  ResetVarsToZero();
+}
+
+/**Disables answer selection */
+function disableSelection(){
+  document.getElementById("answer_a").style.pointerEvents = 'none';
+  document.getElementById("answer_b").style.pointerEvents = 'none';
+  document.getElementById("answer_c").style.pointerEvents = 'none';
+  document.getElementById("answer_d").style.pointerEvents = 'none';
+}
+
+/**Enables answer selection */
+function enableSelection(){
+  document.getElementById("answer_a").style.pointerEvents = 'auto'; 
+  document.getElementById("answer_b").style.pointerEvents = 'auto'; 
+  document.getElementById("answer_c").style.pointerEvents = 'auto'; 
+  document.getElementById("answer_d").style.pointerEvents = 'auto'; 
+}
+
+/**Resets Variables to Zero */
+function ResetVarsToZero() {
+  rightAnsweredQuestions = 0;
+  currentQuestion = 0;
+}
+
+/**displays quiz */
+function showQuiz(){
+  document.getElementById('javascript-quiz').classList.remove('d-none');
+  document.getElementById("quiz-start").classList.remove("d-none");
+}
+
+/**Resets the selection after each question */
 function resetCard() {
   document.getElementById("answer_1").parentNode.classList.remove("bg-success");
   document.getElementById("answer_1").parentNode.classList.remove("bg-danger");
@@ -272,105 +286,13 @@ function resetCard() {
   document.getElementById("answer_4").parentNode.classList.remove("bg-danger");
 }
 
-function restartGameB() {
-  document.getElementById("quiz-start").classList.remove("d-none");
-  document.getElementById("quiz-end").classList.add("d-none");
-  rightAnsweredQuestions = 0;
-  currentQuestion = 0;
-  initB();
-}
-
+/**displays Endscreen */
 function showEndScreen(){
-    AUDIO_END.play();
-    document.getElementById("quiz-start").classList.add("d-none");
-    document.getElementById("quiz-end").classList.remove("d-none");
-    document.getElementById("quiztotal-end").innerHTML = questionsB.length;
-    document.getElementById("end-score-correct-answers").innerHTML =  rightAnsweredQuestions;
-}
-
-function returnToStart(){
-  document.getElementById("quiz-end").classList.add("d-none");
+  AUDIO_END.play();
   document.getElementById("quiz-start").classList.add("d-none");
-  document.getElementById("javascript-quiz").classList.add("d-none");
-  document.getElementById("start-quiz").classList.remove("d-none");
-  rightAnsweredQuestions = 0;
-  currentQuestion = 0;
-}
-
-function updateProgressBarB(){
-  let percent = (currentQuestion) / questionsB.length;
-  percent = percent * 100;
-  document.getElementById("progress-bar").innerHTML = `${percent}%`;
-  document.getElementById("progress-bar").style = `width: ${percent}%`;
-}
- 
-function updateQuestionB(){
-  document.getElementById("quiztotal").innerHTML = questionsB.length;
-  document.getElementById("current-question").innerHTML = currentQuestion + 1;
-}
-
-function displayCurrentQuestion(question){
-  document.getElementById("questiontext").innerHTML = question["question"];
-  document.getElementById("answer_1").innerHTML = question["answer_1"];
-  document.getElementById("answer_2").innerHTML = question["answer_2"];
-  document.getElementById("answer_3").innerHTML = question["answer_3"];
-  document.getElementById("answer_4").innerHTML = question["answer_4"];
-}
-
-function gameIsOverB(){
-  return currentQuestion >= questionsB.length;
-}
-
-function disableSelection(){
-  document.getElementById("answer_a").style.pointerEvents = 'none';
-  document.getElementById("answer_b").style.pointerEvents = 'none';
-  document.getElementById("answer_c").style.pointerEvents = 'none';
-  document.getElementById("answer_d").style.pointerEvents = 'none';
-}
-
-function enableSelection(){
-  document.getElementById("answer_a").style.pointerEvents = 'auto'; 
-  document.getElementById("answer_b").style.pointerEvents = 'auto'; 
-  document.getElementById("answer_c").style.pointerEvents = 'auto'; 
-  document.getElementById("answer_d").style.pointerEvents = 'auto'; 
-}
-
-///////////////////////////////////////////////////////////////////////////////////GAMES QUIZ END//////////////////////////////////////////////////////////////////////////////////////
-
-function showQuiz(){
-  document.getElementById('javascript-quiz').classList.remove('d-none');
-  document.getElementById("quiz-start").classList.remove("d-none");
-}
-
-/**initiates JS Quiz */
-function SelectQuizA(){
-  document.getElementById('start-quiz').classList.add('d-none');
-  initA();
-  AUDIO_BEGIN.play();
-}
-/**initiates Games Quiz */
-function SelectQuizB(){
-  document.getElementById('start-quiz').classList.add('d-none');
-  initB();
-  AUDIO_BEGIN.play();
-}
-
-/**selects JS Quiz */
-function selectA(){
-  document.getElementById("quiz-button").setAttribute( "onClick", "SelectQuizA()" );
-  document.getElementById("next-question").setAttribute( "onClick", "nextQuestionA()" );
-  document.getElementById("restart-button").setAttribute( "onClick", "restartGameA()" );
-  document.getElementById("JS").classList.add('selected');
-  document.getElementById("GM").classList.remove('selected');
-}
-/**selects Games Quiz */
-function selectB(){
-  document.getElementById("quiz-button").setAttribute( "onClick", "SelectQuizB()" );
-  document.getElementById("next-question").setAttribute( "onClick", "nextQuestionB()" );
-  document.getElementById("restart-button").setAttribute( "onClick", "restartGameB()" );
-  document.getElementById("GM").classList.add('selected');
-  document.getElementById("JS").classList.remove('selected');
-  document.getElementById("quiz-type").innerHTML = "Games quiz wurde beendet";
+  document.getElementById("quiz-end").classList.remove("d-none");
+  document.getElementById("quiztotal-end").innerHTML = questionsA.length;
+  document.getElementById("end-score-correct-answers").innerHTML =  rightAnsweredQuestions;
 }
 
 
